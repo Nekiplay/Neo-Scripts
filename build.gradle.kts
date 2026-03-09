a9c147c06d7845d8a4e7d30f7141305a8cef48f7
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2026-03-09 22:13:22 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,10 +103,10 @@ dependencies {
 
     modRuntimeOnly(files("libs/firmament.jar"))
 
-    include(implementation("ai.djl:api:0.25.0")!!)
-    include(implementation("ai.djl:basicdataset:0.25.0")!!)
+    include(implementation("ai.djl:api:0.21.0")!!)
+    include(implementation("ai.djl:basicdataset:0.21.0")!!)
 
-    include(implementation("ai.djl.pytorch:pytorch-engine:0.25.0")!!)
+    include(implementation("ai.djl.pytorch:pytorch-engine:0.21.0")!!)
     include(implementation("ai.djl.pytorch:pytorch-native-auto:1.9.1")!!)
 }
 
