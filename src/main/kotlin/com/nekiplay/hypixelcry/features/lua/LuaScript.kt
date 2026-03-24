33d9d19583650a8dedceb8eff67f07402e7d04d5
Roman Danilov <neci.03@mail.ru>
2026-03-25 03:34:12 +0500

    Update LuaScript.kt

--- src/main/kotlin/com/nekiplay/hypixelcry/features/lua/LuaScript.kt
+++ src/main/kotlin/com/nekiplay/hypixelcry/features/lua/LuaScript.kt
@@ -1394,6 +1394,11 @@ class LuaScript(val scriptName: String, private val luaManager: LuaManager) {
                         wrapper.push()
 
                         val result = L.pCall(1, 0)
+                        if (status != 0.toLong()) { // В некоторых версиях iroiro это Long/Int
+                            val errorMsg = L.toString(-1) ?: "Unknown Lua Error"
+                            HypixelCry.LOGGER.error("${HypixelCry.LOG_PREFIX}Error in world render callback: $errorMsg")
+                            L.pop(1) // ОБЯЗАТЕЛЬНО удаляем сообщение об ошибке
+                        }
                     } catch (e: Exception) {
                         HypixelCry.LOGGER.error("${HypixelCry.LOG_PREFIX}Error in world render callback: ${e.message}")
                     }
@@ -1421,6 +1426,11 @@ class LuaScript(val scriptName: String, private val luaManager: LuaManager) {
                 renderContext.push()
 
                 val result = L.pCall(1, 0)
+                if (status != 0.toLong()) { // В некоторых версиях iroiro это Long/Int
+                    val errorMsg = L.toString(-1) ?: "Unknown Lua Error"
+                    HypixelCry.LOGGER.error("${HypixelCry.LOG_PREFIX}Error in 2D render callback in: $errorMsg")
+                    L.pop(1) // ОБЯЗАТЕЛЬНО удаляем сообщение об ошибке
+                }
             } catch (e: Exception) {
                 HypixelCry.LOGGER.error("${HypixelCry.LOG_PREFIX}Error in 2D render callback in ${scriptName}: ${e.message}")
             } finally {
