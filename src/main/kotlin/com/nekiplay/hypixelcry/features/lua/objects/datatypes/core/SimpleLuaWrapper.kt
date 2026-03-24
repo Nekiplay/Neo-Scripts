6755edd7e7c694d9e1f6bfef4806dd728a054100
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2026-03-25 00:21:43 +0500

    Update SimpleLuaWrapper.kt

--- src/main/kotlin/com/nekiplay/hypixelcry/features/lua/objects/datatypes/core/SimpleLuaWrapper.kt
+++ src/main/kotlin/com/nekiplay/hypixelcry/features/lua/objects/datatypes/core/SimpleLuaWrapper.kt
@@ -9,45 +9,25 @@ abstract class SimpleLuaWrapper(val L: Lua) {
     open fun setFieldValue(l: Lua, key: String, value: LuaValue): Boolean = false
 
     open fun push(): LuaValue {
-        L.newTable() 
+        L.newTable()
         val tableIdx = L.getTop()
 
-        // Store this Java object inside the table so the metatable can find it
         L.pushJavaObject(this)
         L.setField(tableIdx, "__java_instance")
 
-        // Create the metatable
         L.newTable()
-
-        // __index: Called when you do object.key
         L.push(JFunction { l ->
             l.getField(1, "__java_instance")
             val wrapper = l.toJavaObject(-1) as? SimpleLuaWrapper
-            l.pop(1) // remove java instance from stack
-
+            l.pop(1)
             val key = l.toString(2) ?: ""
             l.smartPush(wrapper?.getFieldValue(l, key))
             1
         })
         L.setField(-2, "__index")
 
-        // __newindex: Called when you do object.key = value
-        L.push(JFunction { l ->
-            l.getField(1, "__java_instance")
-            val wrapper = l.toJavaObject(-1) as? SimpleLuaWrapper
-            l.pop(1)
-
-            val key = l.toString(2) ?: ""
-            l.pushValue(3)
-            val value = l.get()
-            wrapper?.setFieldValue(l, key, value)
-            0
-        })
-        L.setField(-2, "__newindex")
-
         L.setMetatable(tableIdx)
-        return L.get() // Returns the table as a LuaValue and cleans stack
-    }
+        return L.get() 
 }
 
 fun Lua.smartPush(v: Any?) {
