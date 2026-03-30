138b88032e8710124b438af42d4dbd576a4c2572
Roman Danilov <neci.03@mail.ru>
2026-03-30 22:49:54 +0500

    Update LuaCommand.kt

--- src/main/kotlin/com/nekiplay/hypixelcry/features/commands/impl/LuaCommand.kt
+++ src/main/kotlin/com/nekiplay/hypixelcry/features/commands/impl/LuaCommand.kt
@@ -88,7 +88,7 @@ object LuaCommand {
     }
 
     private fun openScriptsFolder(source: FabricClientCommandSource) {
-        val scriptsDir = File(Minecraft.getInstance().gameDirectory, "/config/hypixelcry/scripts")
+        val scriptsDir = Minecraft.getInstance().gameDirectory.resolve("config/hypixelcry/scripts")
 
         if (!scriptsDir.exists()) {
             scriptsDir.mkdirs()
