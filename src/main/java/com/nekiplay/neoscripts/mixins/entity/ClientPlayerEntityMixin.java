64a26e600f7baa782c78c914abfb5f85a954faeb
Roman Danilov <neci.03@mail.ru>
2026-04-17 00:22:24 +0500

    Update ClientPlayerEntityMixin.java

--- src/main/java/com/nekiplay/neoscripts/mixins/entity/ClientPlayerEntityMixin.java
+++ src/main/java/com/nekiplay/neoscripts/mixins/entity/ClientPlayerEntityMixin.java
@@ -1,11 +1,19 @@
 package com.nekiplay.neoscripts.mixins.entity;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mojang.authlib.GameProfile;
 import com.nekiplay.neoscripts.events.SendMovementPacketsEvent;
+import com.nekiplay.neoscripts.utils.RaycastUtils;
+import com.nekiplay.neoscripts.utils.Rotations;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.ClientInput;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,6 +22,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LocalPlayer.class)
 public abstract class ClientPlayerEntityMixin extends AbstractClientPlayer {
+
+
     @Shadow
     public ClientInput input;
 
@@ -27,8 +37,30 @@ public abstract class ClientPlayerEntityMixin extends AbstractClientPlayer {
     private void onSendMovementPacketsHead(CallbackInfo ci) {
         SendMovementPacketsEvent.PRE.invoker().onSendMovementPacketsPre(getYRot(), getXRot());
     }
+
     @Inject(method = "sendPosition", at = @At("TAIL"))
     private void onSendMovementPacketsTail(CallbackInfo info) {
         SendMovementPacketsEvent.POST.invoker().onSendMovementPacketsPost();
+    }
+
+    @ModifyExpressionValue(method = "pick(Lnet/minecraft/world/entity/Entity;DDF)Lnet/minecraft/world/phys/HitResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;pick(DFZ)Lnet/minecraft/world/phys/HitResult;"))
+    private static HitResult hookRaycast(HitResult original, Entity camera, double blockInteractionRange, double entityInteractionRange, float tickDelta) {
+        if (camera != Minecraft.getInstance().player) {
+            return original;
+
+        }
+
+        if (Rotations.rotating) {
+            return RaycastUtils.findCrosshairTarget(camera,
+                    camera.getEyePosition(),
+                    Rotations.serverYaw,
+                    Rotations.serverPitch,
+                    ((Player) camera).blockInteractionRange(),
+                    ((Player) camera).entityInteractionRange()
+            );
+        }
+        else {
+            return original;
+        }
     }
 }
