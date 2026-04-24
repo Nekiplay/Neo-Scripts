1ecb30d1b1f8a7ba897d88f3554f3a2a6194c51e
Roman Danilov <neci.03@mail.ru>
2026-04-25 03:38:23 +0500

    Update WindowMixin.java

--- src/main/java/com/nekiplay/neoscripts/mixins/WindowMixin.java
+++ src/main/java/com/nekiplay/neoscripts/mixins/WindowMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.main.GameConfig;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,12 +23,14 @@ public class WindowMixin {
     @Final
     private Window window;
 
+    @Unique
     private final ArrayList<String> startUpScriptNames = new ArrayList<String>() {{
         add("autoload.lua");
         add("startup.lua");
         add("init.lua");
     }};
 
+    @Unique
     private void loadStartupScripts(File dir) {
         // Автозагрузка скриптов при старте
         for (String name : startUpScriptNames) {
