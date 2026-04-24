85ec5cdffdd5bd4c28d6c50585d3c788e48cdcde
Roman Danilov <neci.03@mail.ru>
2026-04-25 04:03:58 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ val lwjgl_version: String by project
 plugins {
     java
     `maven-publish`
-    id("fabric-loom-remap") version "1.15-SNAPSHOT"
+    id("fabric-loom") version "1.15-SNAPSHOT"
     id("org.jetbrains.kotlin.jvm") version "2.2.21"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
     id("com.gradleup.shadow") version "9.3.0"
