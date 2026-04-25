ea17034e8e0fcd88eb732527ffffa13e01e347fd
Roman Danilov <neci.03@mail.ru>
2026-04-25 05:35:29 +0500

    Update Event.kt

--- src/main/kotlin/com/nekiplay/neoscripts/events/main/Event.kt
+++ src/main/kotlin/com/nekiplay/neoscripts/events/main/Event.kt
@@ -2,7 +2,4 @@ package com.nekiplay.neoscripts.events.main
 
 import com.nekiplay.neoscripts.utils.DirectionalInput
 
-abstract class Event {
-    @JvmField
-    open var directionalInput: DirectionalInput = TODO("initialize me")
-}
+abstract class Event {}
