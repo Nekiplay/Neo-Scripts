1eb28e61cd2b35e283bb9dd1378d16128628d787
Roman Danilov <neci.03@mail.ru>
2026-04-25 05:28:30 +0500

    Update Entity.kt

--- src/main/kotlin/com/nekiplay/neoscripts/sugar/Entity.kt
+++ src/main/kotlin/com/nekiplay/neoscripts/sugar/Entity.kt
@@ -1,11 +1,12 @@
 package com.nekiplay.neoscripts.sugar
 
 import com.nekiplay.neoscripts.Main.mc
+import com.nekiplay.neoscripts.utils.aiming.RotationManager
 import net.minecraft.world.entity.Entity
 
 fun Entity.getRotation(): Pair<Float, Float> {
-    return if (this == mc.player && Rotations.rotating) {
-        Pair(Rotations.serverYaw, Rotations.serverPitch)
+    return if (this == mc.player && !RotationManager.getCurrentYaw().isNaN()) {
+        Pair(RotationManager.getCurrentYaw(), RotationManager.getCurrentPitch())
     }
     else {
         Pair(this.yRot, this.xRotO)
