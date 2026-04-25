01d928bdaeafce859a3d077de2424643115ab4e6
Roman Danilov <neci.03@mail.ru>
2026-04-25 05:59:31 +0500

    Update MixinLocalPlayer.java

--- src/main/java/com/nekiplay/neoscripts/mixins/entity/MixinLocalPlayer.java
+++ src/main/java/com/nekiplay/neoscripts/mixins/entity/MixinLocalPlayer.java
@@ -1,14 +1,20 @@
 package com.nekiplay.neoscripts.mixins.entity;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mojang.authlib.GameProfile;
 import com.nekiplay.neoscripts.events.*;
 import com.nekiplay.neoscripts.events.main.EventBus;
+import com.nekiplay.neoscripts.utils.RaycastUtils;
+import com.nekiplay.neoscripts.utils.aiming.RotationManager;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.ClientInput;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -50,5 +56,39 @@ public abstract class MixinLocalPlayer extends AbstractClientPlayer {
     @Inject(method = "raycastHitResult", at = @At("RETURN"))
     private void raycastPostHitResultHook(float f, Entity entity, CallbackInfoReturnable<HitResult> cir) {
         EventBus.INSTANCE.send(new RaycastHitEvent(false));
+    }
+    @ModifyExpressionValue(method = "pick(Lnet/minecraft/world/entity/Entity;DDF)Lnet/minecraft/world/phys/HitResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;pick(DFZ)Lnet/minecraft/world/phys/HitResult;"))
+    private static HitResult hookRaycast(HitResult original, Entity camera, double blockInteractionRange, double entityInteractionRange, float tickDelta) {
+        if (camera != Minecraft.getInstance().player) {
+            return original;
+
+        }
+
+        if (Float.isNaN(RotationManager.INSTANCE.getCurrentYaw())) {
+            return RaycastUtils.findCrosshairTarget(camera,
+                    camera.getEyePosition(),
+                    RotationManager.INSTANCE.getCurrentYaw(),
+                    RotationManager.INSTANCE.getCurrentPitch(),
+                    ((Player) camera).blockInteractionRange(),
+                    ((Player) camera).entityInteractionRange()
+            );
+        }
+        else {
+            return original;
+        }
+    }
+
+    @ModifyExpressionValue(method = "pick(Lnet/minecraft/world/entity/Entity;DDF)Lnet/minecraft/world/phys/HitResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getViewVector(F)Lnet/minecraft/world/phys/Vec3;"))
+    private static Vec3 hookRotationVector(Vec3 original, Entity camera, double blockInteractionRange, double entityInteractionRange, float tickDelta) {
+        if (camera != Minecraft.getInstance().player) {
+            return original;
+        }
+
+        if (Float.isNaN(RotationManager.INSTANCE.getCurrentYaw())) {
+            return Vec3.directionFromRotation(RotationManager.INSTANCE.getCurrentYaw(), RotationManager.INSTANCE.getCurrentPitch());
+        }
+        else {
+            return original;
+        }
     }
 }
