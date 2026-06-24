4ba5c8ad5288058f4ab816293d09008c517f33ef
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2026-06-24 17:44:03 +0500

    Update LuaEvents.kt

--- fabric/src/main/kotlin/neoscripts/features/modules/impl/misc/LuaEvents.kt
+++ fabric/src/main/kotlin/neoscripts/features/modules/impl/misc/LuaEvents.kt
@@ -213,17 +213,9 @@ object LuaEvents : ClientModule() {
                     packet.change.position.z
                 }
 
-                val activeYaw = if (RotationManager.getCurrentYaw().isNaN()) {
-                    player?.yRot ?: 0f
-                } else {
-                    RotationManager.getCurrentYaw()
-                }
+                val activeYaw = player?.yRot ?: 0f
 
-                val activePitch = if (RotationManager.getCurrentPitch().isNaN()) {
-                    player?.xRot ?: 0f
-                } else {
-                    RotationManager.getCurrentPitch()
-                }
+                val activePitch = player?.xRot ?: 0f
 
                 // Проверяем наличие фактических изменений
                 val rotationChanged = player == null || targetYaw != activeYaw || targetPitch != activePitch
