b93673f2df2d2d21c164404debfcba52e0162d43
lmoboy <steamgoyu19@gmail.com>
2026-07-14 15:36:15 +0300

    hopefully fix wrongful style var pushes

--- neoforge/src/main/kotlin/com/nekiplay/neoscripts/features/lua/objects/misc/ImGuiLib.kt
+++ neoforge/src/main/kotlin/com/nekiplay/neoscripts/features/lua/objects/misc/ImGuiLib.kt
@@ -1485,8 +1485,12 @@ class ImGuiLib(val script: LuaScript) : LuaValue() {
         override fun invoke(args: Varargs): Varargs {
             val idx = args.checkint(1)
             val x = args.checkdouble(2).toFloat()
-            val y = if (args.narg() > 2) args.checkdouble(3).toFloat() else 0.0f
-            ImGui.pushStyleVar(idx, x, y)
+            if (args.narg() > 2){
+                ImGui.pushStyleVar(idx, x)                
+            } else {
+                val y = args.checkdouble(3).toFloat()
+                ImGui.pushStyleVar(idx, x, y)
+            }
             return NIL
         }
     }
